dac6bcacc5c0f8c02affbfaf41bc278716a3cf29
Nickylood <110985074+Nickylood@users.noreply.github.com>
2023-03-26 10:53:28 +0200

    123

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,29 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import "flatpickr/dist/flatpickr.min.css";
+
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const searchElements = element => document.querySelector(element);
+const form = searchElements('.form');
+const step = searchElements('[name="step"]');
+const amount = searchElements('[name="amount"]');
+const delay = searchElements('[name="delay"]');
+
+
+function createPromise(position, delayNumber) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve ({position, delayNumber});
+      } else {
+        reject ({position, delayNumber});
+      }
+    }, delayNumber);
+  })
+}
+
+function stepAmountDelayPromise(e) {
+  e.preventDefault();
+
+  
 }
